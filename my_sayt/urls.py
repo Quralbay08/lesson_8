5a3550f265d0bdd13b0a333e6d8fc132d2637764
Quralbay08 <quralbayorinbaev@gmail.com>
2024-08-24 17:34:56 +0500

    my blog sayt

--- my_sayt/urls.py
+++ my_sayt/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('',views.homepage,name='home'),
+    path('',homepage,name='home'),
+    path('reg/',registration,name='reg'),
+    path('login/',sing_in,name='login'),
+    path('logout/',log_out,name='logout'),
+    path('detail/<int:id>',product_detail,name='detail'),
+    path('create/',create_post,name='create'),
+    path('update/<int:id>',update_post,name='update'),
     
-]
+    ]
--- my_sayt/views.py
+++ my_sayt/views.py
@@ -1,15 +1,90 @@
 from . import models
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
+
+from . import forms
+from django.contrib.auth import login,logout
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+
+from django.contrib.auth.decorators import login_required
 
 
 def homepage(request):
     blog = models.Blogs.objects.all()
     return render(request,'home.html',{'bloglar':blog})
 
-def product_detail(request):
-    blog = models.Blogs.objects.all()
-    return render(request,'detail.html',{'detail':blog})
+def product_detail(request,id):
+    blog_d = get_object_or_404(models.Blogs,id=id)
+    return render(request,'detail.html',{'detail':blog_d})
 
+
+def registration(request):
+    if request.method =='POST':
+        form  = forms.RegForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return redirect('home')
+    else:
+        form  = forms.RegForm()
+    return render(request,'reg.html',{'form':form})
+
+
+def sing_in(request):
+    if request.method =='POST':
+        form  = forms.LoginForm(request,data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request,user)
+            return redirect('home')
+    else:
+        form  = forms.LoginForm()
+    return render(request,'login.html',{'form':form})
+
+def log_out(request):
+    logout(request)
+    return redirect('home')
+
+
+@login_required
+
+def create_post(request):
+    if request.method=='POST':
+        form=forms.BlogForm(request.POST,request.FILES)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.author=request.user
+            form.save()
+            return redirect('home')
+    else:
+        form=forms.BlogForm()
+    return render(request,'create_post.html',{'form':form})
+
+
+@login_required
+def update_post(request,id):
+    blog_post = get_object_or_404(models.Blogs,id=id)
+    if blog_post.author!=request.user:
+        return redirect('home')
+    model = models.Blogs.objects.get(id=id)
+    form = forms.BlogForm(request.POST or None, request.FILES,instance=model)
+    if form.is_valid():
+            form.save()
+            return redirect('home')
+    return render(request,'update_post.html',{'form':form,'model':model})
+
+
+@login_required
+def delete_post(request,id):
+    blog_post = get_object_or_404(models.Blogs,id=id)
+    if blog_post.author!=request.user:
+        return redirect('home')
+    model = models.Blogs.objects.get(id=id)
+    form = forms.BlogForm(request.POST or None, request.FILES,instance=model)
+    if request.method=='POST':
+        model.delete()
+        return redirect('home')
+    return render(request,'delete.html',{'form':form})
 
 
 
